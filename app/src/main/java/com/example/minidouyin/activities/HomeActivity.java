4ffb70122627f23269b37a6ecfdd580c33ea5a4c
Junqi Lu <jq_lu@outlook.com>
2019-07-22 12:18:04 +0800

    实现了摇一摇进入拍摄的功能

--- app/src/main/java/com/example/minidouyin/activities/HomeActivity.java
+++ app/src/main/java/com/example/minidouyin/activities/HomeActivity.java
@@ -1,23 +1,74 @@
 package com.example.minidouyin.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.WindowManager;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.minidouyin.fragments.HomeFragment;
 
-public class HomeActivity extends AbstractSingleFragmentActivity {
+public class HomeActivity extends AbstractSingleFragmentActivity implements SensorEventListener{
 
     private static final String TAG = "HomeActivity";
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 2000;
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
 
 
     @Override
     protected Fragment createFragment() {
         Log.d(TAG, "createFragment: ");
 
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
         return new HomeFragment();
     }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    startActivity(new Intent(HomeActivity.this, RecordActivity.class));
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 
 }
